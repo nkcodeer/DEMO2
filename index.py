f9c276ad9e5a053054a7f31403167d31413c4b89
nkcodeer <nikhil958990@gmail.com>
2025-02-13 22:46:48 +0530

    tt

--- index.py
+++ index.py
@@ -63,4 +63,9 @@ print(thislist)
 thislist = ["apple", "banana", "cherry"]
 thislist.insert(1, "orange")
 print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+print(thislist)
  
