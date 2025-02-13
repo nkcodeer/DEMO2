3019390e2650e75e2634fef64c5d0d30e04c525b
nkcodeer <nikhil958990@gmail.com>
2025-02-13 22:40:53 +0530

    adding list

--- index.py
+++ index.py
@@ -15,4 +15,52 @@ def myfunc():
   print("localscope" , y)
 
 myfunc()
-print("checking in global scope"  , y)
+#print("checking in global scope"  , y)
+
+'''
+List [] items are ordered, changeable, and allow duplicate values.
+
+List items are indexed, the first item has index [0], the second item has index [1] etc
+
+list are changangle and allow duplicate values
+'''
+'''
+if age < 10 then print "baby", if age between 10 to 20 then print school boy, if age more than 20 then print men
+'''
+print("enter age")
+age = input()
+age= int(age)
+
+if age < 10:
+  print('baby')
+elif age > 10 and age < 20:
+  print('school boy')
+else:
+  print('men')
+
+#print 1 to 5
+#how to itterate over a string
+print(" enter name")
+name = input()
+for x in name:
+  print("printing x here",x)
+  print(name)
+  print("name")
+
+  #list
+
+  thislist = list(("apple", "banana", "cherry"))
+print(thislist)
+print(len(thislist))
+print(thislist[0])
+for x in thislist:
+ print(x)
+#To add an item to the end of the list, use the append() method
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+print(thislist)
+#The insert() method inserts an item at the specified inde
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(1, "orange")
+print(thislist)
+ 
