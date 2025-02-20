c8e61bd8f4ffbba4744fe9908bdf590b72b563e1
nkcodeer <nikhil958990@gmail.com>
2025-02-20 10:21:48 +0530

    programs

--- dict.py
+++ dict.py
@@ -4,6 +4,33 @@ print(student["name"])
 #creating dict of 5 student and marks
 # Creating a dictionary of students with their marks
 students_marks = {"Ankul": 85,"Rahul": 90,"Piyush": 78,"Shivam": 92,"Ankit": 88}
-
-# Print the marks of a specific student
-print("Marks of Rahul:", students_marks["Rahul"])
+print("the marks of rahul is ", students_marks["Rahul"])
+#delete 
+students_marks.pop("Ankul")
+print(students_marks)
+#check if key exists
+for x in students_marks:
+    print(x)
+#Write a function that checks whether a given key exists in a dictionary.
+def key_exists(dictionary, key):
+    return key in dictionary
+my_dict = {"Ankul": 85,"Rahul": 90,"Piyush": 78,"Shivam": 92,"Ankit": 88}
+key_check = "Shivam"
+if key_exists(my_dict, key_check):
+    print(f"The key '{key_check}' exists in the dictionary.")
+else:
+    print(f"The key '{key_check}' does not exist.")
+# Write a Python program to iterate through all keys and values of a dictionary and print them.
+for x, y in my_dict.items(): #items() method is used to iterate through the dictionary, which returns key-value pairs.
+ print(f"{x}:{y}") 
+ '''f" = The f in print(f"{key}: {value}") indicates that the string is an f-string, 
+ which stands for "formatted string literal. '''
+#Write a Python program to merge two dictionaries into one.
+merged_dict = {**student,**my_dict}
+print(merged_dict)
+#max and min value in dict
+max_value = max(my_dict.values())
+min_value = min(my_dict.values())
+print(max_value)
+print(min_value)
+#Invert a Dictionary (Swap Keys and Values)
---
+++ programs.py
@@ -0,0 +1,74 @@
+#check a no is prime
+def is_prime(n): #The function is_prime(n) is defined to determine if a given number n is a prime number.
+    '''The first condition if n <= 1: checks if the number is less than or equal to 1.If n is 1 or less, 
+    the function immediately returns False because prime numbers are defined as greater than 1.'''
+    if n <= 1:
+        return False
+    '''Iterate Over Possible Divisors:
+
+    The for loop for i in range(2, n): iterates over all integers i starting from 2 up to n-1.
+    This loop is used to check if n is divisible by any number other than 1 and itself.'''
+    for i in range(2, n):
+        '''Check for Divisibility:
+        Inside the loop, the condition if n % i == 0: checks if n is divisible by i.
+        The expression n % i calculates the remainder when n is divided by i.
+        If the remainder is 0, it means n is divisible by i, indicating that n is not a prime number.'''
+        if n % i == 0:
+            '''If n is divisible by any i in the loop, the function returns False immediately, 
+            indicating that n is not a prime number.'''
+            return False
+    '''If the loop completes without finding any divisors (i.e., n is not divisible by 
+    any number from 2 to n-1),  the function returns True.
+   This means n is a prime number because it has no divisors other than 1 and itself.'''
+
+    return True
+# Get input from the user
+user_input = input("Enter a number: ")
+
+# Convert the input to an integer
+number = int(user_input)
+
+# Check if the number is prime and print the result
+result = is_prime(number)
+print(result)
+
+#divisible by 3
+def is_divible(n):
+    if n % 3 ==0:
+        return True
+    else:
+       return False
+user_input = input("enter a number : ")
+number = int(user_input)
+result = is_divible(number)
+print(result)
+
+#binary search
+def binary_search_iterative(arr, target):
+    # Define the search bounds
+    left, right = 0, len(arr) - 1  
+    while left <= right:
+        # Calculate the middle index
+        mid = left + (right - left) // 2  
+        # If the middle element is the target, return its index
+        if arr[mid] == target:
+            return mid  
+        # If the target is bigger, narrow the search to the right half
+        elif arr[mid] < target: 
+            left = mid + 1  
+        # If the target is smaller, narrow the search to the left half
+        else: 
+            right = mid - 1  
+    # Return -1 if the target is not found
+    return -1 
+
+arr = [2, 3, 4, 10, 40]
+target = 10
+
+    
+# Run the iterative function
+result = binary_search_iterative(arr, target)
+if result != -1:
+    print(f"Iterative: Target found at index {result}")
+else:
+    print("Iterative: Target not found")
